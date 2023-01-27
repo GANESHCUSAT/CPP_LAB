26394742f905e5696c179a6cb1792f3adf9dbca9
GANESHCUSAT <117643249+GANESHCUSAT@users.noreply.github.com>
2023-01-27 10:14:09 +0530

    Add files via upload

--- kagk_oop_set1/question3.cpp
+++ kagk_oop_set1/question3.cpp
@@ -3,33 +3,25 @@ using namespace std;
 class bank
 {
 private:
-    string static cust_name;
-    int acc_no;
+    string cust_name;
+    int acc_no, amount;
     char acc_type;
+    int balance;
     int static count;
-    int static balance;
 
 public:
-    bank(int a)
-    {
-        acc_no = a;
-    }
-    bank()
-    {
-    }
-    int new_acc(int acc_no);
-    int withdraw(int acc_no, int amount, bank);
-    int deposit(int acc_no, int amount, bank);
-    int balance_enquiry(int acc_no);
-    int account_statement(int acc_no);
+    void new_acc(void);
+    void withdraw(void);
+    void deposit(void);
+    void balance_enquiry(void);
+    void account_statement(void);
 };
 int bank::count;
-int bank::balance;
 
-int bank::new_acc(int acc_no)
+void bank::new_acc()
 {
-    int a;
-    a = acc_no;
+    count++;
+    acc_no = count;
     cout << "enter your name: ";
     cin >> cust_name;
     cout << "enter the type of the account\nenter S for savings\nenter c for cureent account\nenter: ";
@@ -39,14 +31,15 @@ int bank::new_acc(int acc_no)
     cout << "your account had been created and the account number is :" << acc_no;
 }
 
-int bank::withdraw(int acc_no, int amount, bank customer)
+void bank::withdraw()
 {
-    balance = customer.balance_enquiry(acc_no);
+    cout << "Enter the amount you have to withdraw\n";
+    cin >> amount;
     if ((balance - amount) > 500)
     {
         balance = balance - amount;
         cout << "Rs." << amount << "is withdrawn";
-        cout << "\nyour cureent balance=" << balance;
+        cout << "\nyour current balance=" << balance;
     }
     else
     {
@@ -54,34 +47,34 @@ int bank::withdraw(int acc_no, int amount, bank customer)
     }
 }
 
-int bank::deposit(int acc_no, int amount, bank customer)
+void bank::deposit()
 {
-    balance = customer.balance_enquiry(acc_no);
+    cout << "Enter the amount you have to deposit\n";
+    cin >> amount;
     balance = balance + amount;
     cout << "your current balance is:" << balance;
 }
 
-int bank::balance_enquiry(int acc_no)
+void bank::balance_enquiry()
 {
-    // cout << "current balance is" << balance;
-    return balance;
+    cout << "current balance is" << balance;
 }
 
-int bank::account_statement(int acc_no)
+void bank::account_statement()
 {
     cout << "********\n";
-    cout << "Your Account Statement is \n";
-    cout << "Account Holder  : " << cust_name;
-    cout << "Account No      : " << acc_no;
-    cout << "Account Type    : " << acc_type;
-    cout << "Account Balance : Rs." << balance;
-    cout << "*********\n";
+    cout << "Your Account Statement is ";
+    cout << "\nAccount Holder  : " << cust_name;
+    cout << "\nAccount No      : " << acc_no;
+    cout << "\nAccount Type    : " << acc_type;
+    cout << "\nAccount Balance : Rs." << balance;
+    cout << "\n*********\n";
 }
 int main()
 {
-    int acc_number, b, option, amount, acc_no, count = 0;
-
-    string customer, choice, an, acc;
+    int option, amount, acc_no, count = 0;
+    string choice;
+    bank *customer = new bank[100];
 
     do
     {
@@ -92,58 +85,43 @@ int main()
         if (option == 1)
         {
 
-            count++;
-            acc_no = count;
-            an = acc_no
-            acc = to_string(an);
-            customer = "customer" + acc;
-            bank customer;
-            customer.new_acc(acc_no);
+            customer[count + 1].new_acc();
         }
         else if (option == 2)
         {
             cout << "kindly please enter your account number: ";
-            cin >> acc_number;
-            cout << "enter the amount to be withdrawn: ";
-            cin >> amount;
-            an=acc_number;
-            acc = to_string(an);
-            customer = "customer" + acc;
-            bank customer;
-            customer.withdraw(acc_number, amount, customer);
+            cin >> acc_no;
+            for (int i = acc_no; i == acc_no; i++)
+            {
+                customer[i].withdraw();
+            }
         }
         else if (option == 3)
         {
             cout << "kindly please enter your account number: ";
-            cin >> acc_number;
-            cout << "enter the amount to be deposit: ";
-            cin >> amount;
-            an=acc_number;
-            acc = to_string(an);
-            customer = "customer" + acc;
-            bank customer;
-            customer.deposit(acc_number, amount, customer);
+            cin >> acc_no;
+            for (int i = acc_no; i == acc_no; i++)
+            {
+                customer[i].deposit();
+            }
         }
         else if (option == 4)
         {
             cout << "kindly please enter your account number: ";
-            cin >> acc_number;
-            an=acc_number;
-            acc = to_string(an);
-            customer = "customer" + acc;
-            bank customer;
-            b = customer.balance_enquiry(acc_number);
-            cout << "your cureent balance is:" << b;
+            cin >> acc_no;
+            for (int i = acc_no; i == acc_no; i++)
+            {
+                customer[i].balance_enquiry();
+            }
         }
         else if (option == 5)
         {
             cout << "kindly please enter your account number: ";
-            cin >> acc_number;
-            an = acc_number;
-            acc = to_string(an);
-            customer = "customer" + acc;
-            bank customer;
-            customer.account_statement(acc_number);
+            cin >> acc_no;
+            for (int i = acc_no; i == acc_no; i++)
+            {
+                customer[i].account_statement();
+            }
         }
         else
         {
